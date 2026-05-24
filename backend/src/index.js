a1dac23c5cf9a204858cc6bed8fb37105ba40bdd
infinity-Noah <nikhil950833@gmail.com>
2026-05-25 02:14:33 +0530

    fix wildcard

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,15 +7,14 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-// Serve React frontend
 app.use(express.static(path.join(__dirname, "../../public")));
 
-app.get('/api/hello', (req,res) => {
+app.get('/api/hello', (req, res) => {
     res.send("Hello from the backend")
 })
 
-// All other routes serve the React app
-app.get('*', (req, res) => {
+// Fix for Express 5
+app.get('/{*splat}', (req, res) => {
     res.sendFile(path.join(__dirname, "../../public", "index.html"));
 });
 
