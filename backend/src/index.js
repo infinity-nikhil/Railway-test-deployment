933aa1fff7ef2622b86d65ca9f49a762ebb170fd
infinity-Noah <nikhil950833@gmail.com>
2026-05-25 12:31:03 +0530

    public folder error fix

--- backend/src/index.js
+++ backend/src/index.js
@@ -7,15 +7,17 @@ const __dirname = path.dirname(__filename);
 
 const app = express();
 
-app.use(express.static(path.join(__dirname, "../../public")));
+// Log the path so we can debug
+console.log("Public path:", path.join(__dirname, "../public"));
+
+app.use(express.static(path.join(__dirname, "../public")));
 
 app.get('/api/hello', (req, res) => {
     res.send("Hello from the backend")
 })
 
-// Fix for Express 5
 app.get('/{*splat}', (req, res) => {
-    res.sendFile(path.join(__dirname, "../../public", "index.html"));
+    res.sendFile(path.join(__dirname, "../public", "index.html"));
 });
 
 app.listen(3000, () => console.log("Server started"));
